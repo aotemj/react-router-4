08504c209a7d7f6bacae70b87078c33af1799543
zhaoxinlei <991807272@qq.com>
2018-07-15 22:26:03 +0800

    #初始化#

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { Router, Route } from 'react-router';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+
+      </div>
+    );
+  }
+}
+
+export default App;
